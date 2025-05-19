6baabade104c92a25224210d0f934819755e2987
yurika tsuji <sakurasou.tystu@gmail.com>
2025-05-19 20:58:21 +0900

    打刻機能追加による修正

--- src/resources/views/registration.blade.php
+++ src/resources/views/registration.blade.php
@@ -11,7 +11,7 @@
 @section('header')
 <nav class="header-nav">
     <ul class="header-nav__list">
-        <li><a href="" class="header-nav__attendance">勤怠</a></li>
+        <li><a href="/attendance" class="header-nav__attendance">勤怠</a></li>
         <li><a href="" class="header-nav__list">勤怠一覧</a></li>
         <li><a href="" class="header-nav__application">申請</a></li>
         <li>
@@ -27,26 +27,46 @@
 @section('content')
 <div class="content">
     <div class="stamp-item">
+        <p class="item__status">{{ $status }}</p>
 
-        <p class="item__status">勤務外</p>
-        <!-- <p class="item__status">出勤中</p>
-        <p class="item__status">休憩中</p>
-        <p class="item__status">退勤済</p> -->
-
-        <p class="item__date">2025年5月5日(木)</p>
-        <p class="item__time">08:00</p>
+        <p class="item__date">{{ $now->format('Y年m月d日') }}</p>
+        <p class="item__time">{{ $now->format('H:i') }}</p>
 
         <!-- 出勤ボタン -->
-        <button class="item__button">出勤</button>
+        @if($clockIn)
+        <form action="/attendance/clockIn" method="post">
+            @csrf
+            <button class="item__button" type="submit">出勤</button>
+        </form>
+        @endif
         <!-- 休憩入り時ボタン -->
-        <!-- <div class="stamp-button">
-            <button class="item__button">退勤</button>
-            <button class="item__button--break">休憩入</button>
-        </div> -->
+        <div class="stamp-button">
+            @if($clockOut)
+            <form action="/attendance/clockOut" method="post">
+                @csrf
+                <button class="item__button">退勤</button>
+            </form>
+            @endif
+            @if($breakStart)
+            <form action="/attendance/breakStart" method="post">
+                @csrf
+                <button class="item__button--break">休憩入</button>
+            </form>
+            @endif
+        </div>
+
         <!-- 休憩終わり時ボタン -->
-        <!-- <button class="item__button--break">休憩戻</button> -->
+        @if($breakEnd)
+        <form action="/attendance/breakEnd" method="post">
+            @csrf
+            <button class="item__button--break">休憩戻</button>
+        </form>
+        @endif
+
         <!-- 退勤ボタン -->
-        <!-- <p class="end-message">お疲れ様でした。</p> -->
+        @if($clockOutMessage)
+        <p class="end-message">お疲れ様でした。</p>
+        @endif
 
     </div>
 </div>
